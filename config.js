f85356858cf7c763be4b5992a9d59161b997b5b8
benjay10 <ben.behaeghe@redpencil.io>
2024-10-07 16:30:16 +0200

    Allow overruling organisation graph check on read
    This was needed on the Toezicht app because there are no organisation
    graphs.

--- config.js
+++ config.js
@@ -5,6 +5,11 @@ export const GRAPH_TEMPLATE = env.get('GRAPH_TEMPLATE')
   .default('http://mu.semte.ch/graphs/organizations/~ORGANIZATION_ID~/LoketLB-toezichtGebruiker')
   .asUrlString();
 
+export const OVERRULE_ORG_CHECK_ON_READ = env.get('OVERRULE_ORG_CHECK_ON_READ')
+  .example('false')
+  .default('false')
+  .asBool();
+
 (function checkEnvVars() {
   if (!/~ORGANIZATION_ID~/g.test(GRAPH_TEMPLATE))
     throw new Error(`The GRAPH_TEMPLATE environment variable ${GRAPH_TEMPLATE} does not contain a ~ORGANIZATION_ID~.`);
